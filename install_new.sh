0fe4c565aa481dba1cfe79a1d1aca64cff9758c8
0 <42121860+x00x0x00x@users.noreply.github.com>
2020-02-25 16:36:23 +0100

    Create install_new.sh

---
+++ install_new.sh
@@ -0,0 +1,23 @@
+cd /home/
+mkdir ubuntu
+cd ubuntu
+sudo ufw allow 9444/tcp
+sudo ufw allow 9446/udp
+sudo apt update
+sudo apt install haveged -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install supervisor -y
+git clone https://github.com/YanDevDe/nyzoVerifier.git
+cd nyzoVerifier
+./gradlew build
+sudo mkdir -p /var/lib/nyzo/production
+sudo cp trusted_entry_points /var/lib/nyzo/production
+sudo cp nyzoVerifier.conf /etc/supervisor/conf.d
+######################### args: nickname
+echo $1 > /var/lib/nyzo/production/nickname
+#########################
+sudo supervisorctl reload
+sudo supervisorctl status
+echo "@reboot sudo supervisorctl reload" >> mycron
+crontab mycron
+rm mycron
